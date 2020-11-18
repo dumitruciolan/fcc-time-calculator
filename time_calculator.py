866cc8b1811747f5349d2ff9f4d65b8310f1eec7
Dumitru Ciolan <dumitru.ciolan@outlook.com>
2020-11-18 14:05:54 +0000

    Initial commit

---
+++ time_calculator.py
@@ -0,0 +1,15 @@
+def add_time(start, duration, day=None):
+  modifiers_later = 0
+  days_later = 0
+  days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  modifier = start.split(" ")[1]
+  initial_modifier = modifier
+
+  start = start.split(" ")
+  start.pop(1)
+  start + ''.join(start)
+
+
+
+  return new_time
